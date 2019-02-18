cb3617d30270f2c6835d311861f83f255c668757
Kevin Hunter <kevin@kevin-hunter.net>
2019-02-17 22:09:29 -0500

    Adding in the main project and moving in the services

---
+++ SOCVR.AutoCommentsServer/Services/Abstract/ICommentDataTransformer.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SOCVR.AutoCommentsServer.Services.Abstract
+{
+    public interface ICommentDataTransformer
+    {
+        string ConvertMarkdownToJsonp(string markdownContent);
+    }
+}
---
+++ SOCVR.AutoCommentsServer/Settings/GitSettings.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SOCVR.AutoCommentsServer.Settings
+{
+    public class GitSettings
+    {
+        public string Url { get; set; }
+        public string Branch { get; set; }
+        public string CloneDir { get; set; }
+    }
+}
